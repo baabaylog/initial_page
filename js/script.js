b7de86f8790eb6a50fd9f78f33dc5dead59c788d
baabaylog <baabaylog@gmail.com>
2024-04-21 20:04:37 +0500

    js Event

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-function submenuButton(e){
-    console.log(e)
-}
+
+document.querySelectorAll('.menu-items ul li').forEach((val, index, elemets)=>{
+    val.addEventListener('click', function (){
+        for (let i = 0; i < elemets.length; i++) {
+            const ele = elemets[i];
+            ele.classList.remove('active')
+        }
+        val.classList.add('active')
+        if(val.dataset.value && val.dataset.value == 'dashboard'){
+            document.querySelector('.dashboard').classList.remove('hide');
+        }else{
+            document.querySelector('.dashboard').classList.add('hide');
+        }
+    })
+
+});
